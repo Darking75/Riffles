45d7d5093e2f3608db19b93f55e9460fef7fe4b2
Alejandro <xiao1357@gmail.com>
2023-01-09 16:39:50 -0500

    Initial Commit

---
+++ Rifas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rifas
+//
+//  Created by Alejandro Torres on 9/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
